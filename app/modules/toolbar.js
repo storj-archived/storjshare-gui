1d640c75569f6c6466dab91f26a5d801dbd78ebe
Tome Boshevski <tome@boshevski.com>
2015-09-22 21:13:42 +0200

    Remove old commands

--- app/modules/toolbar.js
+++ app/modules/toolbar.js
@@ -14,10 +14,6 @@ exports.initToolbar = function() {
 			{ type: 'button', id: 'settings', caption: 'Preferences', icon: 'fa fa-cog' },
 			{ type: 'button',  id: 'farm',  caption: 'Farm', icon: 'fa fa-cogs' },
 			{ type: 'button',  id: 'cancel',  caption: 'Cancel', icon: 'fa fa-ban' },
-			{ type: 'spacer',  id: 'spacer0' },
-			{ type: 'button',  id: 'register',  caption: 'Register', icon: 'fa fa-user-plus' },
-			{ type: 'button',  id: 'build',  caption: 'Build', icon: 'fa fa-wrench' },
-			{ type: 'button',  id: 'poll',  caption: 'Poll', icon: 'fa fa-cloud-upload' },
 		],
 		onClick: function (event) {
 			switch (event.target) {
@@ -28,33 +24,6 @@ exports.initToolbar = function() {
 					$(document).trigger('clearGridRecords');
 					$(document).trigger('terminateProcess');
 					$(document).trigger('farm', [
-						function (output) {
-							$(document).trigger('addGridRecord', output.toString());
-						}
-					]);
-					break;
-				case 'build':
-					$(document).trigger('clearGridRecords');
-					$(document).trigger('terminateProcess');
-					$(document).trigger('build', [
-						function (output) {
-							$(document).trigger('addGridRecord', output.toString());
-						}
-					]);
-					break;
-				case 'register':
-					$(document).trigger('clearGridRecords');
-					$(document).trigger('terminateProcess');
-					$(document).trigger('register', [
-						function (output) {
-							$(document).trigger('addGridRecord', output.toString());
-						}
-					]);
-					break;
-				case 'poll':
-					$(document).trigger('clearGridRecords');
-					$(document).trigger('terminateProcess');
-					$(document).trigger('poll', [
 						function (output) {
 							$(document).trigger('addGridRecord', output.toString());
 						}
