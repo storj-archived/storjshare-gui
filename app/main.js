e021215554863db57c47d1c5962e22b8a8263474
gamedevsam <sambatista1@gmail.com>
2015-10-13 09:30:34 -0400

    Making window slightly larger.

--- app/main.js
+++ app/main.js
@@ -11,7 +11,7 @@ var mainWindow;
 // Preserver of the window size and position between app launches.
 var mainWindowState = windowStateKeeper('main', {
 	width: 800,
-	height: 675
+	height: 700
 });
 
 app.on('ready', function () {
