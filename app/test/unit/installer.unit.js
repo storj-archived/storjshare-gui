edc90a6733ea4994bdd135bc7dc87b04ef4dcbf4
Gordon Hall <gordon@gordonwritescode.com>
2015-12-07 17:36:04 -0500

    scaffold installed unit tests

---
+++ app/test/unit/installer.unit.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var proxyquire = require('proxyquire');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var Installer = require('../../lib/installer');
+
+describe('DataServInstaller', function() {
+
+  describe('@constructor', function() {
+
+
+
+  });
+
+  describe('#install', function() {
+
+
+
+  });
+
+  describe('#check', function() {
+
+
+
+  });
+
+  describe('#getDataServClientPath', function() {
+
+
+
+  });
+
+  describe('#_installGnuLinux', function() {
+
+
+
+  });
+
+  describe('#_installMacintosh', function() {
+
+
+
+  });
+
+  describe('#_installWindows', function() {
+
+
+
+  });
+
+  describe('#_checkGnuLinux', function() {
+
+
+
+  });
+
+  describe('#_checkMacintosh', function() {
+
+
+
+  });
+
+  describe('#_checkWindows', function() {
+
+
+
+  });
+
+  describe('#_checkPythonPipGnuLinux', function() {
+
+
+
+  });
+
+  describe('#_getDownloadURL', function() {
+
+
+
+  });
+
+  describe('#_getDownloadStream', function() {
+
+
+
+  });
+
+  describe('#_downloadAndExtract', function() {
+
+
+
+  });
+
+});
