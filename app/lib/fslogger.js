2936525fc1b4e8f1ddc4f440373559ce53906af8
aleitner <leitnersalex@gmail.com>
2016-06-30 16:01:15 -0400

    Updating unit tests for constructor

--- app/lib/fslogger.js
+++ app/lib/fslogger.js
@@ -19,7 +19,7 @@ function FsLogger(logfolder) {
 
   //need to set defaults
   this._loglevel = 3;
-  this._logfolder = logfolder ? logfolder : require('os').tmpdir();
+  this._logfolder = (logfolder != null) ? logfolder : require('os').tmpdir();
   this._logfile = this._useExistingFile();
 
 
--- app/test/unit/fslogger.unit.js
+++ app/test/unit/fslogger.unit.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var expect = require('chai').expect;
+var proxyquire = require('proxyquire');
 var FsLogger = require('../../lib/fslogger');
+var sinon = require('sinon');
 
 describe('FsLogger', function() {
 
@@ -12,6 +14,42 @@ describe('FsLogger', function() {
 
     it('should create an instance with the `new` keyword', function() {
       expect(new FsLogger()).to.be.instanceOf(FsLogger);
+    });
+
+    it('should start with default logging level and locations', function() {
+      var logger = new FsLogger();
+      expect(logger._loglevel).to.not.be.null;
+      expect(logger._loglevel).to.equal(3);
+
+      expect(logger._logfolder).to.not.be.null;
+      expect(logger._logfolder).to.not.equal('');
+
+      expect(logger._logfile).to.not.be.null;
+      expect(logger._logfile).to.not.equal('');
+    });
+
+    it('should use the os tmp folder if no parameters are passed', function() {
+      var logger = new FsLogger();
+      expect(logger._logfolder).to.equal(require('os').tmpdir());
+    });
+
+    it('should use the log folder passed to FsLogger for files', function() {
+      var StubbedLogger = proxyquire('../../lib/fslogger', {
+        fs: {
+          existsSync: sinon.stub().returns(true)
+        },
+        _doesFileExist: sinon.stub().returns(true),
+        _useExistingFile: sinon.stub().returns("/fake/file.log"),
+        _newfile: sinon.stub()
+      });
+      var path = 'path/to/logs'
+      var logger = new StubbedLogger(path);
+      expect(logger._logfolder).to.equal(path);
+    });
+
+    it('should create a new log file if log does not exist', function() {
+      var logger = new FsLogger();
+      //asset newfile was called
     });
   });
 
