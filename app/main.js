54ccad4c8928cbc280b8603d5712744bdfd8e824
aleitner <leitnersalex@gmail.com>
2016-07-05 16:50:37 -0400

    Changed window size for new settings modal

--- app/main.js
+++ app/main.js
@@ -48,7 +48,7 @@ app.on('ready', function() {
   var menu = new ApplicationMenu();
   main = new BrowserWindow({
     width: 500,
-    height: PLATFORM === 'mac' ? 600 : 635
+    height: PLATFORM === 'mac' ? 712 : 747
   });
 
   sysTray = new SysTrayIcon(
