3c5ccea369ac34a304e5fabaaa1b4c41684422c8
aleitner <leitnersalex@gmail.com>
2016-08-17 16:18:24 -0400

    Updated post install script

--- tasks/postinstall.sh
+++ tasks/postinstall.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pushd ./app/node_modules/leveldown && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
-popd
+cd ./app/node_modules/leveldown && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
