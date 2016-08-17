62775782bfb5c37ce23dc7bff0f54a42744e2e48
aleitner <leitnersalex@gmail.com>
2016-08-17 16:12:23 -0400

    Updated post install script

--- tasks/postinstall.sh
+++ tasks/postinstall.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-ls
-pushd ./app/node_modules/storj/node_modules/leveldown && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
-popd
-pushd ./../../../leveldown/ && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
+pushd ./app/node_modules/leveldown && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
 popd
