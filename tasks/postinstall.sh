739a2970006b868f2bedfb67c00b25826630931c
aleitner <leitnersalex@gmail.com>
2016-08-17 15:20:13 -0400

    Bumped Storj dependency package

--- tasks/postinstall.sh
+++ tasks/postinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-cd ./app/node_modules/storj/node_modules/leveldown && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
-cd ./../../../leveldown/ && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
+ls
+pushd ./app/node_modules/storj/node_modules/leveldown && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
+popd
+pushd ./../../../leveldown/ && node-gyp rebuild --target=1.2.6 --dist-url=https://atom.io/download/atom-shell
+popd
