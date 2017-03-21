e9f4e2b6d89a95d32384bb3783220aa516d578be
MrFancyMonocle <ryanforan@gmail.com>
2017-03-21 18:33:20 -0400

    adding bandaid for error state in new shares

--- app/views/share-wizard/index.js
+++ app/views/share-wizard/index.js
@@ -5,13 +5,20 @@ module.exports = {
     'error': require('../notification')
   },
   data: function() {
-    return window.Store.newShare;
+    return {
+      newShare: window.Store.newShare,
+      shareList: window.Store.shareList
+    };
   },
   template: `
 <transition name="fade">
   <section>
     <transition name="fade">
-      <error class="error-stream alert alert-danger alert-dismissible" v-bind:notes="errors" v-bind:dismiss-action="actions.clearErrors"></error>
+      <error class="error-stream alert alert-danger alert-dismissible" v-bind:notes="newShare.errors" v-bind:dismiss-action="newShare.actions.clearErrors"></error>
+    </transition>
+    
+    <transition>
+      <error class="error-stream alert alert-danger alert-dismissible" v-bind:notes="shareList.errors" v-bind:dismiss-action="shareList.actions.clearErrors"></error>
     </transition>
 
     <router-view></router-view>
