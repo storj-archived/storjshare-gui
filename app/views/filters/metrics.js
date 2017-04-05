5c41b6fd6b1a36b9c1bd585f9d5413e9f14ad08e
MrFancyMonocle <ryanforan@gmail.com>
2017-04-05 18:18:29 -0400

    adding some simple precision handling for disk allocator

--- app/views/filters/metrics.js
+++ app/views/filters/metrics.js
@@ -3,7 +3,7 @@ const convert = require('bytes');
 
 module.exports = {
   toUnit: function(bytes, unit) {
-    return convert(Number(bytes), {unit: unit});
+    return convert(Number(bytes), {unit: unit, decimalPlaces: 4});
   },
   toBytes: function(str) {
     return convert.parse(String(str));
